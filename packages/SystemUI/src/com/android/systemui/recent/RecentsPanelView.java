47a77c1c4cf8dede636fd73b09c8d2d028afca79
Daveee10 <daver16@msn.com>
2013-07-06 19:53:34 +0200

    Fix Recent Ram bar calculation

--- packages/SystemUI/src/com/android/systemui/recent/RecentsPanelView.java
+++ packages/SystemUI/src/com/android/systemui/recent/RecentsPanelView.java
@@ -123,7 +123,7 @@ public class RecentsPanelView extends FrameLayout implements OnItemClickListener
     private boolean mFirstScreenful = true;
     private boolean mHighEndGfx;
 
-    private long mTotalUsedMemory;
+    private long mFreeMemory;
     private long mTotalMemory;
     private long mCachedMemory;
     private long mActiveMemory;
@@ -1040,8 +1040,8 @@ public class RecentsPanelView extends FrameLayout implements OnItemClickListener
                         freeMem = mTotalMemory - mActiveMemory - mCachedMemory;
                         break;
                     case 3:
-                        usedMem = mActiveMemory + mCachedMemory + mTotalUsedMemory;
-                        freeMem = mTotalMemory - mActiveMemory - mCachedMemory - mTotalUsedMemory;
+                        usedMem = mTotalMemory - mFreeMemory;
+                        freeMem = mFreeMemory;
                         break;
                 }
 
@@ -1055,7 +1055,7 @@ public class RecentsPanelView extends FrameLayout implements OnItemClickListener
                 mRamText.setText(getResources().getString(
                         R.string.memory));
                 float totalMem = mTotalMemory;
-                float totalShownMem = mTotalUsedMemory / totalMem;
+                float totalShownMem = (mTotalMemory - mFreeMemory - mCachedMemory -  mActiveMemory) / totalMem;
                 float totalActiveMem = mActiveMemory / totalMem;
                 float totalCachedMem = mCachedMemory / totalMem;
                 mRamUsageBar.setRatios(totalShownMem, totalCachedMem, totalActiveMem);
@@ -1126,7 +1126,7 @@ public class RecentsPanelView extends FrameLayout implements OnItemClickListener
                 }
             }
         } catch (IOException e) {}
-        mTotalUsedMemory = result;
+        mFreeMemory = result;
 
         try {
             String firstLine = readLine("/proc/meminfo", 6);
